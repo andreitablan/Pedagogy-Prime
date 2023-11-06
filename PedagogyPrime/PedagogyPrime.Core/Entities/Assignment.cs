86a92e74b80f3c2caef3e7902692c172ea395a49
Andrie Tablan <andreitablan01@gmail.com>
2023-11-07 00:11:14 +0200

    Implementation of the base entities
    Assignment and Homework entities added.

---
+++ PedagogyPrime/PedagogyPrime.Core/Entities/Assignment.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System;
+
+namespace PedagogyPrime.Core.Entities
+{    
+    public class Assignment: BaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid CourseId { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Description { get; set; }
+        public List<Homework> Homeworks { get; set; }
+        public Homework Solution { get; set; }
+    }
+}
---
+++ PedagogyPrime/PedagogyPrime.Core/Entities/Homework.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace PedagogyPrime.Core.Entities
+{
+    public enum Grade
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten
+    };
+    public class Homework : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Content { get; set; }
+        public Grade Grade { get; set; }
+        public string Review { get; set; }
+        public string FirebaseLink { get; set; }
+    }
+}
