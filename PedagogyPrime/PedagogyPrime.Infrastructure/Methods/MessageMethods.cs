74fe595f86498f06802273c4a28a8222ac8f2db9
SavRares <savrarem@gmail.com>
2023-11-07 17:20:27 +0200

    Added methods for Message, Subject and Forum

---
+++ PedagogyPrime/PedagogyPrime.Infrastructure/Methods/MessageMethods.cs
@@ -0,0 +1,15 @@
+﻿using PedagogyPrime.Core.Entities;
+
+namespace PedagogyPrime.Infrastructure.Methods
+{
+    //TODO (SR)
+    //This is the message to be sent in the forums.
+    public class MessageMethods
+    {
+        // CRUD methods
+        // Create, Read, Update, Delete Subject.
+        // Other methods
+        public void EditMessage(Guid idMessage) { }
+
+    }
+}
