4a6e3d7d9dfad1ac619aca1a1fdc6cbcd7c08c83
DELL125\Rares Opariuc <opariucraresioan@gmail.com>
2023-11-07 11:30:43 +0200

    Added my base classes and added the Methods folder

--- PedagogyPrime/PedagogyPrime.Core/Entities/Course.cs
+++ PedagogyPrime/PedagogyPrime.Core/Entities/Course.cs
@@ -2,5 +2,9 @@
 {
     public class Course : BaseEntity
     {
-    }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Content { get; set; }
+        public Double Coverage { get; set; }
+        public String Subject { get; set; }
 }
